e1e71780205ecdcd0a0cc90ccaf8a0749724dd6f
PranjalMantri <pranjalmantri@gmail.com>
2024-01-31 14:39:13 +0530

    Created Search Bar

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// console.log("Fetching Data");
+
+// const fetchData = async function () {
+//   const res = await fetch("https://api.github.com/users/lucametehau");
+//   const data = await res.json();
+//   return data;
+// };
+
+// (async () => {
+//   const data = await fetchData();
+//   console.log(data);
+//   console.log(data.starred_url);
+//   console.log(data.repos_url);
+//   console.log(data.public_repos);
+// })();
